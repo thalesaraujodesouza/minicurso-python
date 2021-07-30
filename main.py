759dab7d8d5b0e1ef37bce00c09f8ea27375a2ce
thalesaraujodesouza <78521408+thalesaraujodesouza@users.noreply.github.com>
2021-07-30 11:52:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-import pandas as pd
+import pandas asimport pandas as pd
+import openpyxl
+
+# importar a base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
+# visualizar a base de dados
+pd.set_option('display.max_columns', None)
+
+# faturamento por loja
+faturamento = tabela_vendas[["ID Loja", "Valor Final"]].groupby("ID Loja").sum()
+#print(faturamento)
+
+# quantidade de produtos vendidos por loja
+quantidade = tabela_vendas[["ID Loja", "Quantidade"]].groupby("ID Loja").sum()
+#print(quantidade)
+
+# ticket médio por produto em cada loja
+ticketMedio = tabela_vendas[["ID Loja", "Produto", "Valor Unitário"]].groupby("ID Loja").median()
+print(ticketMedio)
+# enviar um email com o relatório pd
 import openpyxl
 
 # importar a base de dados
